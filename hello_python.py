f3e4df41855749045d31922b4971741f1862ba92
Matthew Austin Lewis <matthewaustinlewis@icloud.com>
2025-06-25 17:53:44 -0400

    Add hello_python.py for MIT Intro to CS course

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 25 17:49:47 2025
+
+@author: matthewaustinlewis
+"""
+
+print("Hello, world! This is my first MIT CS file under Git control.")
